be770f97e22483c285df14e9989f0333e89a08ab
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2018-10-21 02:50:12 +0300

    decode: reorganized mod.rs

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -1,11 +1,15 @@
 mod decoder;
-pub use self::decoder::TextDecoder;
-
 mod error;
-pub use self::error::{DecodeError, RecoveryInfo};
-
-mod utf8dec;
-pub use self::utf8dec::Utf8Decoder;
 
 mod utf16dec;
+mod utf8dec;
+
+// Interfaces
+pub use self::{
+    decoder::TextDecoder,
+    error::{DecodeError, RecoveryInfo},
+};
+
+// Decoders
 pub use self::utf16dec::Utf16Decoder;
+pub use self::utf8dec::Utf8Decoder;
