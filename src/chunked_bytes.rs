715607baa6d9f462bb422a5b2055f1f409968ec4
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2018-11-15 06:02:13 +0200

    Fix ChunkedBytes::reserve
    Make the computations sensible and not prone to overflows.

--- src/chunked_bytes.rs
+++ src/chunked_bytes.rs
@@ -1,6 +1,9 @@
 use bytes::{BufMut, Bytes, BytesMut};
 
-use std::collections::VecDeque;
+use std::{
+    cmp::{max, min},
+    collections::VecDeque,
+};
 
 pub struct ChunkedBytes {
     current: BytesMut,
@@ -17,10 +20,17 @@ impl ChunkedBytes {
     }
 
     pub fn reserve(&mut self, additional: usize) {
-        if self.current.remaining_mut() + additional > self.chunk_size {
+        // If the current buffer has been taken from, its capacity
+        // can be smaller than the chunk size. If a large capacity request
+        // has been reserved, it can be larger. So we use the least of the two
+        // as the limit for appending to the current buffer.
+        let cap = min(self.current.capacity(), self.chunk_size);
+        let written_len = self.current.len();
+        let required = written_len.checked_add(additional).expect("overflow");
+        if required > cap {
             self.flush();
+            self.current.reserve(max(additional, self.chunk_size));
         }
-        self.current.reserve(additional);
     }
 
     pub fn append_chunk(&mut self, chunk: Bytes) {
